ef5158a80521b0252ab7e0d64d1d5e6e47b15efa
linhcao022 <88888624+linhcao022@users.noreply.github.com>
2022-01-10 20:07:12 +0700

    Add files via upload

---
+++ DbConnect.cs
@@ -0,0 +1,84 @@
+﻿using ManagementMoney.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace ManagementMoney
+{
+        public static class DBConnect
+        {
+            public async static void InitializeDatabase()
+            {
+                await Windows.Storage.ApplicationData.Current.LocalFolder.CreateFileAsync("sqliteMoney.db",
+                    Windows.Storage.CreationCollisionOption.OpenIfExists);
+
+                string dbPath = System.IO.Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "sqliteMoney.db");
+
+                using (SQLiteConnection connection = new SQLiteConnection($"Data Source={dbPath};Version=3"))
+                {
+                    await connection.OpenAsync();
+                    String tableCommand = "CREATE TABLE IF NOT EXISTS Moneys (sid INTEGER PRIMARY KEY, MoneyCollected NVARCHAR(2048) NULL, Day NVARCHAR(2048) NULL, Spend NVARCHAR(2048) NULL)";
+                    SQLiteCommand createTable = new SQLiteCommand(tableCommand, connection);
+
+                    createTable.ExecuteReader();
+                    connection.Close();
+                }
+            }
+
+            public async static void AddData(string moneyCollected,string day, string spend)
+            {
+                string dbPath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "sqliteMoney.db");
+                using (SQLiteConnection connection = new SQLiteConnection($"Data Source={dbPath};Version=3"))
+                {
+                    await connection.OpenAsync();
+
+                    SQLiteCommand insertCommand = new SQLiteCommand();
+                    insertCommand.Connection = connection;
+
+                    insertCommand.CommandText = "INSERT INTO Moneys VALUES (NULL, @moneyCollected, @day, @spend);";
+                    insertCommand.Parameters.AddWithValue("moneyCollected", moneyCollected);
+                    insertCommand.Parameters.AddWithValue("day", day);
+                    insertCommand.Parameters.AddWithValue("spend", spend);
+
+                    insertCommand.ExecuteReaderAsync();
+
+                    connection.Close();
+                }
+            }
+
+            public async static
+                Task LoadRecordAsync(ObservableCollection<Money> items)
+            {
+                string dbPath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "sqliteMoney.db");
+                using (SQLiteConnection connection = new SQLiteConnection($"Data Source={dbPath};Version=3"))
+                {
+                    await connection.OpenAsync();
+
+                    SQLiteCommand command = new SQLiteCommand("SELECT * FROM Moneys", connection);
+
+                    using (var reader = await command.ExecuteReaderAsync())
+                    {
+                        var moneyCollectedOrdinal = reader.GetOrdinal("MoneyCollected");
+                        var dayOrdinal = reader.GetOrdinal("Day");
+                        var spendOrdinal = reader.GetOrdinal("Spend");
+
+                        while (await reader.ReadAsync())
+                        {
+                            string moneyCollected = reader.GetString(moneyCollectedOrdinal);
+                            string day = reader.GetString(dayOrdinal);
+                            string spend = reader.GetString(spendOrdinal);
+
+                            items.Add(new Money(moneyCollected, day ,spend));
+                        }
+                    }
+                }
+            }
+        
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using ManagementMoney.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ManagementMoney
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public ObservableCollection<Money> moneys { get; set; }
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            DBConnect.InitializeDatabase();
+
+            moneys = new ObservableCollection<Money>();
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            DBConnect.AddData(collect.Text,day.Text, spend.Text);
+            DBConnect.LoadRecordAsync(moneys);
+            Console.WriteLine(moneys.Count);
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            var id = btn.Tag;
+
+            for (var i = 0; i < moneys.Count; i++)
+            {
+                Money money = moneys[i];
+                if (money.id == Int16.Parse(id.ToString()))
+                {
+                    moneys.Remove(money);
+                    break;
+                }
+            }
+
+        }
+
+        private Money editMoney;
+
+        private void editButton_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            var id = btn.Tag;
+
+            for (var i = 0; i < moneys.Count; i++)
+            {
+                Money money = moneys[i];
+                if (money.id == Int16.Parse(id.ToString()))
+                {
+                    editMoney = money;
+                    day.Text = editMoney.day;
+                    collect.Text = editMoney.moneyCollected;
+                    spend.Text = editMoney.spend;
+                    break;
+                }
+            }
+            edit.ShowAsync();
+        }
+
+        private void edit_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+
+        }
+
+        private void edit_Opened(ContentDialog sender, ContentDialogOpenedEventArgs args)
+        {
+            editCollect.Text = editMoney.moneyCollected;
+            editSpend.Text = editMoney.spend;
+            editDay.Text = editMoney.day;
+        }
+    }
+
+}
+
